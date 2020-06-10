93444e183e537faa0316a5c40f091e807f204637
Michael Dotson <thedotson@icloud.com>
2020-06-09 21:06:51 -0500

    hotfix for init

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,10 @@
 import '../styles/main.scss';
 import navbar from './components/navbar';
+import messageBuilder from './components/messageBuilder';
 
 const init = () => {
   navbar.buildNavbar();
+  messageBuilder.messageBuilder();
 };
 
 init();
