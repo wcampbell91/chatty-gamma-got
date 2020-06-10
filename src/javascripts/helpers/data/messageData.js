028099d435e4abd38b35ad2585eb3f168440ce1b
Michael Dotson <thedotson@icloud.com>
2020-06-09 19:46:04 -0500

    added message array and seed data messages for page load

---
+++ src/javascripts/helpers/data/messageData.js
@@ -0,0 +1,31 @@
+const messages = [
+  {
+    user: 'johnsnow420',
+    message: 'Somebody asked me if I wanted Season 8 on Bluray.  I said I dont want it',
+    timestamp: '',
+  },
+  {
+    user: 'joffreySucks',
+    message: 'Seriously, joffery was a punk!',
+    timestamp: '',
+  },
+  {
+    user: 'tyrionRocks69',
+    message: 'Burp',
+    timestamp: '',
+  },
+  {
+    user: 'nearlyheadlessned',
+    message: 'The show was never the same after Season 6...',
+    timestamp: '',
+  },
+  {
+    user: 'thesebootsweremadeforwhitewalking',
+    message: 'That was it?  All those seasons of build up to be stopped by a little gurl?!',
+    timestamp: '',
+  },
+];
+
+const getMessages = () => messages;
+
+export default { getMessages };
