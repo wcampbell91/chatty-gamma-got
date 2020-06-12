50f53b3e1e82f34420654f62e548dec4a4173f6a
Michael Dotson <thedotson@icloud.com>
2020-06-11 19:07:58 -0500

    added settings buttons

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import '../styles/main.scss';
 import navbar from './components/navbar';
 import messageBuilder from './components/messageBuilder';
+import settings from './components/settings';
 
 const init = () => {
   navbar.buildNavbar();
   messageBuilder.messageBuilder();
+  settings.buildSettings();
 };
 
 init();
