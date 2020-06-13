24cdb55554953d689bc29f9beadb7988ecee8e5c
Billy Campbell <wcampbell91@gmail.com>
2020-06-13 13:52:42 -0500

    added events.js

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,12 +3,14 @@ import navbar from './components/navbar';
 import messageBuilder from './components/messageBuilder';
 import settings from './components/settings';
 import clearMessages from './components/clearMessages';
+import events from './components/events';
 
 const init = () => {
   navbar.buildNavbar();
   messageBuilder.messageBuilder();
   settings.buildSettings();
   clearMessages.clearEvent();
+  events.clickEvents();
 };
 
 init();
